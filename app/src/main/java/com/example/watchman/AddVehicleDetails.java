5a1e49d0c511f2fc73fb8fb782f480e3eacf5159
Govinda <govindavashishtha@gmail.com>
2020-06-27 21:31:49 +0530

    added ocr to the project

--- app/src/main/java/com/example/watchman/AddVehicleDetails.java
+++ app/src/main/java/com/example/watchman/AddVehicleDetails.java
@@ -2,10 +2,13 @@ package com.example.watchman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.SignInButton;
 import com.google.firebase.auth.FirebaseAuth;
@@ -18,20 +21,22 @@ import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 public class AddVehicleDetails extends AppCompatActivity {
-    EditText checkIn , vehicleNum , appartmentDetails , name;
+    EditText checkIn , vehicleNum , apartmentDetails , name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_vehicle_details);
         String vehicleID = getIntent().getExtras().getString("VehicleID");
+        Bitmap imageBitmap = (Bitmap) getIntent().getParcelableExtra("ImageBitmap");
 
         final String  currentDateTimeString = DateFormat.getDateTimeInstance()
                 .format(new Date());
-
+        ImageView vehicleImage = findViewById(R.id.car_plate_photo);
+        vehicleImage.setImageBitmap(imageBitmap);
         checkIn = findViewById(R.id.check_in_details);
         vehicleNum = findViewById(R.id.vehicle_number);
         vehicleNum.setText(vehicleID);
-        appartmentDetails = findViewById(R.id.appartment_details);
+        apartmentDetails = findViewById(R.id.appartment_details);
         name = findViewById(R.id.name);
         checkIn.setEnabled(false);
         checkIn.setText(currentDateTimeString);
@@ -40,18 +45,31 @@ public class AddVehicleDetails extends AppCompatActivity {
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String timeStamp = String.valueOf(TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()));
-                HashMap<String , String> map;
-                map = new HashMap<>();
-                map.put("VehicleNumber" , vehicleNum.getText().toString());
-                map.put("CheckInTime" , currentDateTimeString);
-                map.put("AppartmentDetails" , appartmentDetails.getText().toString());
-                map.put("Name", name.getText().toString());
-                FirebaseDatabase.getInstance().getReference().child(FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber()).child(timeStamp).setValue(map);
-                onBackPressed();
+                if(validateInputs()) {
+                    String timeStamp = String.valueOf(TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()));
+                    HashMap<String, String> map;
+                    map = new HashMap<>();
+                    map.put("VehicleNumber", vehicleNum.getText().toString());
+                    map.put("CheckInTime", currentDateTimeString);
+                    map.put("ApartmentDetails", apartmentDetails.getText().toString());
+                    map.put("Name", name.getText().toString());
+                    FirebaseDatabase.getInstance().getReference().child(FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber()).child(timeStamp).setValue(map);
+                    Toast.makeText(getApplicationContext(), "Information Added",
+                            Toast.LENGTH_LONG).show();
+                    onBackPressed();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Please fill up all the details",
+                            Toast.LENGTH_LONG).show();
+                }
             }
         });
 
+    }
+    private boolean validateInputs(){
+      if(vehicleNum.getText().toString().equals("")||apartmentDetails.getText().toString().equals("")||name.getText().toString().equals("")){
+          return false;
+      }
+      return true;
     }
 
     @Override
--- app/src/main/java/com/example/watchman/HomeActivity.java
+++ app/src/main/java/com/example/watchman/HomeActivity.java
@@ -1,31 +1,52 @@
 package com.example.watchman;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.account.WorkAccount;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.vision.text.TextRecognizer;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.document.FirebaseVisionDocumentText;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
     Button inButton;
     Button outButton;
+    Bitmap imageBitmap;
+    ImageView imageView;
+    TextView textView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         String phone = getIntent().getExtras().getString("phone");
+        textView = findViewById(R.id.textView);
 
         inButton=findViewById(R.id.in_button);
         inButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String outputOCR = "DL 5S 5252" ;
-                // todo : add ocr functionality
-               // outputOCR = getTextFromOCR();
-                Intent intent = new Intent(HomeActivity.this, AddVehicleDetails.class);
-                intent.putExtra("VehicleID", outputOCR);
-                startActivity(intent);
+                dispatchTakePictureIntent();
             }
         });
         outButton=findViewById(R.id.out_button);
@@ -39,5 +60,66 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            Log.e("info", "onActivityResult: "+imageBitmap );
+           detectTxt();
+        }
+    }
+
+    private void detectTxt() {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(imageBitmap);
+        FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance()
+                .getOnDeviceTextRecognizer();
+
+        Task<FirebaseVisionText> result =
+                detector.processImage(image)
+                        .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                            @Override
+                            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                                processTxt(firebaseVisionText);
+                                Log.e("output text", "onSuccess: "+firebaseVisionText );
+                            }
+                        })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(getApplicationContext(), "Can't find text...TRY AGAIN",
+                                                Toast.LENGTH_LONG).show();
+                                    }
+                                });
+    }
+
+    private void processTxt(FirebaseVisionText text) {
+        String txt = "";
+        List<FirebaseVisionText.TextBlock> blocks = text.getTextBlocks();
+        if (blocks.size() == 0) {
+            Toast.makeText(getApplicationContext(), "Can't find text...TRY AGAIN",
+                    Toast.LENGTH_LONG).show();
+            dispatchTakePictureIntent();
+        }
+        for (FirebaseVisionText.TextBlock block : text.getTextBlocks()) {
+           txt = txt + block.getText();
+        }
+        Intent intent = new Intent(HomeActivity.this, AddVehicleDetails.class);
+        intent.putExtra("VehicleID", txt);
+        intent.putExtra("imageBitmap" , imageBitmap);
+        startActivity(intent);
     }
 }
